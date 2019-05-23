69a092b3d829f06fd32cc180ff3b8a16367662b9
kadrikaarlop <kadri.kaarlop@gmail.com>
2019-05-23 10:46:59 +0300

    Initial commit

---
+++ src/components/TodoList.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+        Todo list goes here
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "todo-list",
+        data(){
+            return {
+                msg: 'Welcome to Your Vue.js App'
+            }
+        }
+    }
+</script>
+
+<style lang="scss">
+
+</style>

bb9b9b4e6145ce1c2196de2d01cc81504d93b8a9
kadrikaarlop <kadri.kaarlop@gmail.com>
2019-05-23 11:16:44 +0300

    Proov

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
-        Todo list goes here
+        <input type="text" class="todo-input" placeholder="Mis mureks?" v-model="newTodo">
+        <div v-for="todo in todos" :key="todo.id" class=todo-item"></div>
     </div>
 </template>
 
@@ -9,12 +10,33 @@
         name: "todo-list",
         data(){
             return {
-                msg: 'Welcome to Your Vue.js App'
+                newTodo: '',
+                todos: [
+                    {
+                        'id':1,
+                        'title': 'Finish Vue Screencast',
+                        'complete': false,
+                    },
+                    {
+                        'id: 2',
+                        'title': 'Take over world',
+                        'completed': false,
+                    }
+                ]
             }
         }
     }
 </script>
 
 <style lang="scss">
+.todo-input {
+    width: 100%;
+    padding: 10px 18px;
+    font-size: 18px;
+    margin-bottom: 16px;
 
+    &:focus {
+        outline:0;
+    }
+}
 </style>
